795ddb7ada85c8e27f61835de11d253e8a3ccc68
abo1007 <1003208172@qq.com>
2020-05-06 10:22:34 +0800

    update 101

---
+++ imgtrans_101.vue
@@ -0,0 +1,58 @@
+<template id="img-temp">
+  <div id="img-container" :style="newstyle">
+    <img :src="url" id="img-oneself" alt="">
+    <p id="img-content">{{text}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "imgtrans",
+    data(){
+      return{
+        newstyle:''
+      }
+    },
+    props:['url','text','height','width'],
+    methods:{
+      getHeiAndWid(){
+        this.newstyle = 'height:' + this.height + ';width:' + this.width + ';'
+      }
+    },
+    created(){
+      this.getHeiAndWid()
+    }
+  }
+</script>
+
+<style scoped>
+  *{margin:0;padding:0;}
+  #img-container{
+    width:100%;height:100%;
+    overflow: hidden;
+    position:relative;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    background-color:rgb(0,0,0);
+    transition:all 0.4s ease;
+  }
+  #img-container:hover #img-oneself{
+    opacity:0.3;
+    transform:scale(1.3,1.3);
+    transition:all 0.4s ease;
+  }
+  #img-oneself{
+    height:100%;width:100%;transition:all 0.4s ease;
+  }
+  #img-content{
+    width:80%;
+    position:absolute;
+    color:#ffffff;text-align:center;opacity:0;
+    transform:translate(-100%,0);transition:all 0.4s ease;
+  }
+  #img-container:hover #img-content{
+    transform:translate(0,0);transition:all 0.4s ease;
+    opacity:1;
+  }
+</style>
